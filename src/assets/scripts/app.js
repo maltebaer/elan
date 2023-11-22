e1c6c4a137a6d8202e6753a46afeb5328229b1fc
Malte Bär <maltebaer@posteo.de>
2023-11-22 14:45:51 +0100

    refines modal behaviour

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,5 +1,29 @@
 import Alpine from 'alpinejs'
 
+Alpine.data('modal', () => ({
+    open: false,
+
+    toggleModal(el) {
+        this.open ? el.close() : el.showModal()
+        this.cleanup(el)
+    },
+
+    cleanup(el) {
+        this.open = el.open
+        this.toggleBackdropScrolling()
+    },
+
+    toggleBackdropScrolling() {
+        if (this.open) {
+            document.documentElement.style.scrollbarGutter = 'stable'
+            document.documentElement.style.overflowY = 'hidden'
+        } else {
+            document.documentElement.style.scrollbarGutter = ''
+            document.documentElement.style.overflowY = ''
+        }
+    }
+}))
+
 window.Alpine = Alpine
 
 Alpine.start()
