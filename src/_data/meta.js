75cab5bd52da5b56f4fed2a5ffb0f0642bd24c3e
Malte Bär <maltebaer@posteo.de>
2023-12-30 22:25:10 +0100

    updates favicon and opengraph image

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -14,17 +14,6 @@ module.exports = {
     themeBgColor: '#f8f9fa',
     meta_data: {
         opengraph_default: '/assets/images/opengraph-default.jpg', // fallback/default meta image
-        opengraph_default_alt:
-            'Visible content: Eleventy starter based on workflow for Cube CSS, Every Layout, Design Tokens and Tailwind for uitility, based on the concepts explained in buildexcellentwebsit.es ', // alt text for default meta image
-        twitterSite: '', // i.e. @site - twitter profile of the site
-        twitterCreator: '', // i.e. @author -  twitter profile of the site
-        mastodonProfile: '' // i.e. https://front-end.social/@lene - url to your mastodon instance/profile
-    },
-    blog: {
-        // this is for the rss feed
-        name: 'My great Web Development Blog',
-        description:
-            'Tell the word what you are writing about in your blog! It will show up on feed readers.'
     },
     pagination: {
         itemsPerPage: 20
